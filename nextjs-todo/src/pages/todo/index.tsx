765be5bc4e33f0af4c1a6d93a8754945bc081bb2
Suphachai Phetthamrong <suphachaiphetthamrong@gmail.com>
2023-02-24 23:07:53 +0700

    Base Fullstack App

---
+++ nextjs-todo/src/pages/todo/index.tsx
@@ -0,0 +1,16 @@
+export default function Todo() {
+    return (
+        <div>
+            <h1>Todo</h1>
+            <LoginComponent />
+        </div>
+    )
+}
+
+function LoginComponent() {
+    return (
+        <div>
+            <h1>Login</h1>
+        </div>
+    )
+}
